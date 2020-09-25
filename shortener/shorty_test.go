1edf1903aa1865afbc67480f5af3829c3ac01753
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2020-09-25 14:53:04 +0200

    refactor tests with testify framework

--- shortener/shorty_test.go
+++ shortener/shorty_test.go
@@ -2,6 +2,7 @@ package shortener
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,13 +20,13 @@ func TestGetURL(t *testing.T) {
 		gotDest, err := shortServer.GetURL(response, request)
 		wantDest := "http://test.local/"
 
-		assertNoError(t, err)
+		assert.Nil(t, err)
 
 		got := response.Result().StatusCode
 		want := 308 // redirect status code
 
-		assertInt(t, got, want)
-		assertStrings(t, gotDest, wantDest)
+		assert.Equal(t, got, want)
+		assert.Equal(t, gotDest, wantDest)
 	})
 
 	t.Run("Requesting a non-existing shortcut", func(t *testing.T) {
@@ -34,12 +35,11 @@ func TestGetURL(t *testing.T) {
 
 		_, err := shortServer.GetURL(response, request)
 
-		assertError(t, err)
+		assert.NotNil(t, err)
 
 		got := response.Result().StatusCode
 		want := 404
-
-		assertInt(t, got, want)
+		assert.Equal(t, got, want)
 	})
 }
 
@@ -48,8 +48,8 @@ func TestLookup(t *testing.T) {
 		got, err := shortServer.URLs.Lookup("test")
 		want := "http://test.local/"
 
-		assertNoError(t, err)
-		assertSuccessfulLookup(t, got, want)
+		assert.Nil(t, err)
+		assert.Equal(t, got, want)
 	})
 
 	t.Run("Lookup non existing key", func(t *testing.T) {
@@ -57,41 +57,7 @@ func TestLookup(t *testing.T) {
 		_, err := shortServer.URLs.Lookup(url)
 		want := fmt.Sprintf("could not find the proper URL for %s (unknown)", url)
 
-		assertError(t, err)
-		assertStrings(t, err.Error(), want)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), want)
 	})
-}
-
-func assertError(t *testing.T, err error) {
-	t.Helper()
-	if err == nil {
-		t.Fatal("expected error but didn't get any.")
-	}
-}
-
-func assertNoError(t *testing.T, err error) {
-	t.Helper()
-	if err != nil {
-		t.Fatal("have error but didn't expect any!")
-	}
-}
-
-func assertStrings(t *testing.T, got string, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-}
-
-func assertSuccessfulLookup(t *testing.T, got string, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-}
-
-func assertInt(t *testing.T, got int, want int) {
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
 }
